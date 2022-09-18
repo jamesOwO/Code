381d062176afd804fb7f925f6c715a35119ae974
josh <537387@chrome.escg.ac.uk>
2022-09-18 17:46:18 +0100

    add sololearn 23 code

---
+++ sololearn 23 code.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //your code goes here
+            for(int x = 1; x < number + 1; x++)
+            {
+                if (x % 3 == 0)
+                {
+                    Console.WriteLine("*");
+                }
+                else
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
